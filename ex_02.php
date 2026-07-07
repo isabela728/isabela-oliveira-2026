1bb1608fd76d3929de9fd21cf8b9f58a4d17de3f
isabela728 <isabela_oliveira3@estudante.sesisenai.org.br>
2026-07-07 12:19:03 -0300

    add - exercício 02

---
+++ ex_02.php
@@ -0,0 +1,32 @@
+<!-- Uma empresa de tecnologia está desenvolvendo um sistema para tratamento de
+textos.
+Crie uma função chamada inverterTexto() que receba uma string e retorne o texto
+completamente invertido.
+Além disso, exiba a quantidade de caracteres existentes na string original. -->
+
+<?php
+
+function inverterTexto($texto)
+{
+    $texto_invertido = strrev($texto);
+    $quantidade = strlen($texto);
+
+    return [
+        "texto_invertido" => $texto_invertido,
+        "quantidade_caracteres" => $quantidade
+
+    ];
+}
+
+
+$texto = "socorram me subi no onibus em Marrocos";
+
+echo "Texto original: $texto <br>";
+
+$texto_invertido = inverterTexto($texto);
+$quantidade = inverterTexto($texto);
+
+echo "Texto invertido: " . $texto_invertido['texto_invertido'] . "<br>";
+echo "Quantidade de caracteres: " . $texto_invertido['quantidade_caracteres'] . "<br>";
+
+?>
